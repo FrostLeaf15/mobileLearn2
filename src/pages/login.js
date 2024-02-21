11d4e96260b7522807478f575f58662915ab17cc
FrostLeaf15 <85655364+FrostLeaf15@users.noreply.github.com>
2024-02-21 21:33:12 +0700

    update style login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import styles from '../styles/login.module.css';
 
 const Login = () => {
   const [username, setUsername] = useState('');
@@ -11,34 +12,37 @@ const Login = () => {
   };
 
   return (
-    <div className="login-container">
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      <p><a href="/forgot-password">Forgot Password?</a></p>
-      <p>Don't have an account? <a href="/register">Register</a></p>
+    <div className={styles["login-container"]}>
+  <h1>Login</h1>
+  <form onSubmit={handleSubmit}>
+    <div className={styles["form-group"]}>
+      <label htmlFor="username">Username</label>
+      <input
+        type="text"
+        id="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        required
+      />
     </div>
+    <div className={styles["form-group"]}>
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+    </div>
+    <button type="submit" class="submit-button">Submit</button>
+  </form>
+  <div className={styles["link"]}>
+    <p><a href="/forgot-password">Forgot Password?</a></p>
+    <p>Don't have an account? <a href="/register">Register</a></p>
+  </div>
+</div>
+
   );
 };
 
