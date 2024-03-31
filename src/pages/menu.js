76f7455750f70061edfab6b466aa7194c70befb3
FrostLeaf15 <85655364+FrostLeaf15@users.noreply.github.com>
2024-03-31 08:20:10 +0700

    update repair login and menu
    menambahkan icon pada menu, memperbaiki aspek rasio pada halaman login dan menu

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,6 +2,10 @@ import styles from '../styles/menu.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 import imgPic from '../image/image.jpg';
+import thermometerPic from '../image/thermometer.png';
+import distancePic from '../image/distance.png';
+import heavyPic from '../image/scales.png';
+import timePic from '../image/time.png';
 
 function Menu() {
   return (
@@ -10,25 +14,25 @@ function Menu() {
       <div className={styles.gridContainer}>
         <Link href="/page1">
             <div className={styles.gridItem}>
-              <Image src={imgPic} alt="Image1" className={styles.img} />
+              <Image src={thermometerPic} alt="Suhu" className={styles.img} />
               <p>Suhu</p>
             </div>
         </Link>
         <Link href="/page2">
             <div className={styles.gridItem}>
-              <Image src={imgPic} alt="Image2" className={styles.img} />
+              <Image src={distancePic} alt="Jarak" className={styles.img} />
               <p>Jarak</p>
             </div>
         </Link>
         <Link href="/page3">
             <div className={styles.gridItem}>
-              <Image src={imgPic} alt="Image3" className={styles.img} />
+              <Image src={heavyPic} alt="Massa" className={styles.img} />
               <p>Massa</p>
             </div>
         </Link>
         <Link href="/page4">
             <div className={styles.gridItem}>
-              <Image src={imgPic} alt="Image4" className={styles.img} />
+              <Image src={timePic} alt="Waktu" className={styles.img} />
               <p>Waktu</p>
             </div>
         </Link>
